ba752081e82f21aa86c8653f88009fa8ce4c481b
Bogdan Agafonov <bogdan.agafonov@gmail.com>
2026-09-23 12:26:44 +0200

    Remove superseded browser authority workflows

--- packages/mesh-runtime/src/index.ts
+++ packages/mesh-runtime/src/index.ts
@@ -13,11 +13,7 @@ export { MeshHandshakeCodec, type MeshHandshakeFeatures, type MeshHandshakePaylo
 export { BrowserMeshHandshake, BrowserMeshOutgoingHandshake, type BrowserMeshHandshakeConnection, type BrowserMeshHandshakeHost, type BrowserMeshHandshakePeer, type BrowserMeshHandshakeStream, type BrowserMeshOutgoingConnection, type BrowserMeshOutgoingHandshakeHost } from "./browserHandshake"
 export { BrowserMeshDialScheduler, type BrowserDialPeer, type BrowserMeshDialHost } from "./browserDial"
 export { MeshDialCancelled, MeshNodeRestart, isMeshDialNetworkFailure } from "./errors"
-export { BrowserMeshCredentials, type BrowserMeshCredentialsHost, type BrowserMeshOwnerCredential, type BrowserMeshOwnerProfile } from "./browserCredentials"
-export { BrowserMeshInvitations, type BrowserMeshInvitationCredential, type BrowserMeshInvitationEnvelope, type BrowserMeshInvitationsHost } from "./browserInvitations"
-export { BrowserMeshAuthority, BrowserMeshRecovery, BrowserMeshSuccession, type BrowserMeshAuthorityCredential, type BrowserMeshAuthorityHost, type BrowserMeshAuthorityProfile, type BrowserMeshRecoveryGrant, type BrowserMeshRecoveryHost, type BrowserMeshRecoveryPolicy, type BrowserMeshRecoveryVote, type BrowserMeshSuccessionHost } from "./browserAuthority"
 export { BrowserMeshCatalog, type BrowserMeshCatalogCredential, type BrowserMeshCatalogHost } from "./browserCatalog"
-export { BrowserMeshOwnershipTransfer, type BrowserMeshTransferCredential, type BrowserMeshTransferHost, type BrowserMeshTransferPeer, type BrowserMeshTransferProfile, type BrowserMeshTransferRecord } from "./browserOwnership"
 
 export type { RustMeshRuntimeState as MeshRuntimeState }
 export type { RustLiveSessionAction, RustLiveWorkspaceSession } from "@meta-uber/mesh-replication/runtime"
